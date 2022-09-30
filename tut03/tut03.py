a1cbd70e1218c5a5ce1272d76d7196f0cb3ef8d9
Rajan-CE46 <rajankumar201871@gmail.com>
2022-09-30 16:12:21 +0530

    getting desired output file

--- tut03/tut03.py
+++ tut03/tut03.py
@@ -4,7 +4,7 @@ import numpy as np
 # reading input file
 Dataframe = pd.read_excel('input_octant_longest_subsequence.xlsx')
 # finding mean and printing them in the avg column
-mod =5000
+# mod =5000
 Dataframe.at[0, 'U Avg'] = Dataframe['U'].mean()
 Dataframe.at[0, 'V Avg'] = Dataframe['V'].mean()
 Dataframe.at[0, 'W Avg'] = Dataframe['W'].mean()
@@ -37,8 +37,52 @@ for i in range(z):
         Dataframe.at[i, "Octant"] = -4
 
 # removing NaN values
+
+# Finally convertring Dataframe to desired output csv file also removing first indexes
+#     Dataframe.to_excel("output_octant_longest_subsequence.xlsx", index=False)
+
+Dataframe.at[0,' '] = ' '
+Dataframe.at[0,'  '] = 'Count'
+Dataframe.at[0,'     '] = 'Longest Subsquence Length'
+Dataframe.at[0,'      '] = 'Count'
+Dataframe.at[1,'  '] = '+1'
+Dataframe.at[2,'  '] = '-1'
+Dataframe.at[3,'  '] = '+2'
+Dataframe.at[4,'  '] = '-2'
+Dataframe.at[5,'  '] = '+3'
+Dataframe.at[6,'  '] = '-3'
+Dataframe.at[7,'  '] = '+4'
+Dataframe.at[8,'  '] = '-4'
+# Dataframe.iloc[0,0] = '+1'
+# Dataframe.at[1,'n'] = ' '
+list1 = [1,-1,2,-2,3,-3,4,-4]
+list2 = []
+list3 = []
+for n in list1:
+    a = 0
+    list = []
+    for i in range(z-1):
+        if Dataframe.at[i,'Octant'] == n and Dataframe.at[i+1,'Octant'] == n:
+            a = a+1
+        elif Dataframe.at[i,'Octant'] == n and Dataframe.at[i+1,'Octant'] != n:
+            list.append(a+1)
+            a=0
+#     print(list)
+    largest_count = list[0]
+    for counts in list:
+        if counts > largest_count:
+            largest_count = counts
+    list2.append(largest_count)
+    list3.append(list2.count(largest_count))
+for n in range(8):    
+    Dataframe.at[n+1,'     '] = list2[n]
+    Dataframe.at[n+1,'      '] = list3[n]
+#     print(largest_count)
+# print(list2)
+# print(list3)
+
+
 Dataframe = Dataframe.fillna(' ')
 # just printing first 20 data rows
 Dataframe.head(20)
-# Finally convertring Dataframe to desired output csv file also removing first indexes
 Dataframe.to_excel("output_octant_longest_subsequence.xlsx", index=False)
