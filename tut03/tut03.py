5430c20f612a3b9589e27447f95b74181b1a7c0d
Rajan-CE46 <rajankumar201871@gmail.com>
2022-09-30 20:49:46 +0530

    Adding necessary comment and sumbmitting

--- tut03/tut03.py
+++ tut03/tut03.py
@@ -1,3 +1,4 @@
+# importing libraries
 from datetime import datetime
 start_time = datetime.now()
 def octant_longest_subsequence_count():
@@ -38,12 +39,9 @@ def octant_longest_subsequence_count():
             Dataframe.at[i, "Octant"] = 4
         if(Dataframe.at[i, "U'=U-U Avg"] >= 0 and Dataframe.at[i, "V'=V-V Avg"] < 0 and Dataframe.at[i, "W'=W-W Avg"] < 0):
             Dataframe.at[i, "Octant"] = -4
-
-    # removing NaN values
-
-    # Finally convertring Dataframe to desired output csv file also removing first indexes
-    #     Dataframe.to_excel("output_octant_longest_subsequence.xlsx", index=False)
-
+# Inserting additional columns and also putting desired strings in that according to output file
+# First of all leaving a blank column after octant column
+# alsp putting +1,-1 ... at there desired cell
     Dataframe.at[0,' '] = ' '
     Dataframe.at[0,'  '] = 'Count'
     Dataframe.at[0,'     '] = 'Longest Subsquence Length'
@@ -58,6 +56,8 @@ def octant_longest_subsequence_count():
     Dataframe.at[8,'  '] = '-4'
     # Dataframe.iloc[0,0] = '+1'
     # Dataframe.at[1,'n'] = ' '
+    # creating a list1 that is containing octant values
+    # Also creating list2 and list3 these lists are used to store largest subsequence count and no of occurance using append 
     list1 = [1,-1,2,-2,3,-3,4,-4]
     list2 = []
     list3 = []
@@ -67,16 +67,23 @@ def octant_longest_subsequence_count():
         for i in range(z-1):
             if Dataframe.at[i,'Octant'] == n and Dataframe.at[i+1,'Octant'] == n:
                 a = a+1
+                # this a will be keep on adding till the sae octant value is getting
             elif Dataframe.at[i,'Octant'] == n and Dataframe.at[i+1,'Octant'] != n:
+                # Just after we get diff octant value nos of occ is stored in list using appeng
                 list.append(a+1)
-                a=0
+                # then again a =  0 for next cycle
+                # a=0
+                # this list will store number of continuos occurance of octant value w.r.t each octant value
     #     print(list)
+    # now finding largest occurance of each octant and appending these values in list 2
+    # and count of each largest occurance in list 3
         largest_count = list[0]
         for counts in list:
             if counts > largest_count:
                 largest_count = counts
         list2.append(largest_count)
         list3.append(list.count(largest_count))
+        # now finally pribnting them in their desired place
     for n in range(8):    
         Dataframe.at[n+1,'     '] = list2[n]
         Dataframe.at[n+1,'      '] = list3[n]
@@ -88,9 +95,10 @@ def octant_longest_subsequence_count():
     Dataframe = Dataframe.fillna(' ')
     # just printing first 20 data rows
     Dataframe.head(20)
+    # Finally Converting Dataframe to desired output file
     Dataframe.to_excel("output_octant_longest_subsequence.xlsx", index=False)
 
-
+# Checking for pyuthon version
 from platform import python_version
 ver = python_version()
 
@@ -98,8 +106,8 @@ if ver == "3.8.10":
     print("Correct Version Installed")
 else:
     print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
-
+#  calling octant_longest_subsequence_count() function
 octant_longest_subsequence_count()
-
+#   Printing execution time
 end_time = datetime.now()
 print('Duration of Program Execution: {}'.format(end_time - start_time))
